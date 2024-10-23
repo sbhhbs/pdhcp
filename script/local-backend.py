2a93fd1f1b767a1f200c2b997d4f208b178bda0b
sbhhbs <git@sbhhbs.com>
2024-10-23 23:12:17 +0800

    script update

--- script/local-backend.py
+++ script/local-backend.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, json, time, os, datetime
+import sys, json, time, os, datetime, traceback
 
 log_id = 0
 
@@ -40,8 +40,11 @@ while True:
             if not assigned_ip:
                 if dynamic_mac_ip_map.get(mac_address) == preferred_ip:
                     assigned_ip = preferred_ip
+                elif check_ip_in_use(preferred_ip) == False:
+                    assigned_ip = preferred_ip
             if not assigned_ip:
                 def gen_ip_in_pool():
+                    global pool_assign_count
                     pool_assign_count += 1
                     digit_3 = pool_assign_count // 255 + 200
                     digit_4 = pool_assign_count % 255
@@ -70,19 +73,31 @@ while True:
                 'address-lease-time':      300,
                 'bootp-server-address':    '10.65.0.1',
             }
-            with open(f"/script/logs/{datetime.datetime.now()}_{log_id}.json", "w") as f:
-                json.dump({
-                    "req": request,
-                    "resp": response,
-                    "_internal": {
-                        "dynamic_mac_ip_map": dynamic_mac_ip_map,
-                        "pool_assign_count": pool_assign_count,
-                    }
-                }, f, indent=2, ensure_ascii=False)
-                log_id += 1
+            try:
+                with open(f"/script/logs/{datetime.datetime.now()}_{log_id}.json", "w") as f:
+                    json.dump({
+                        "req": request,
+                        "resp": response,
+                        "_internal": {
+                            "dynamic_mac_ip_map": dynamic_mac_ip_map,
+                            "pool_assign_count": pool_assign_count,
+                        }
+                    }, f, indent=2, ensure_ascii=False)
+                    log_id += 1
+            except Exception as e:
+                ...
             if os.environ.get("DHCP_BACKUP"):
                 time.sleep(10)
             sys.stdout.write(json.dumps(response) + '\n')
             sys.stdout.flush()
-    except:
+    except Exception as e:
+        with open(f"/script/logs/exception_{datetime.datetime.now()}_{log_id}.json", "w") as f:
+            json.dump({
+                "exc": str(e),
+                "trace": traceback.format_exc(),
+                "_internal": {
+                    "dynamic_mac_ip_map": dynamic_mac_ip_map,
+                    "pool_assign_count": pool_assign_count,
+                }
+            }, f, indent=2, ensure_ascii=False)
         break
